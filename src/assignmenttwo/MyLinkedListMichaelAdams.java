95e1dad395d025b4b85e7fd6bb3a62f97ea4fb90
Michael Adams <me@michdavidadams.com>
2022-02-05 13:17:04 -0500

    linkedlist removeLast() fixed

--- src/assignmenttwo/MyLinkedListMichaelAdams.java
+++ src/assignmenttwo/MyLinkedListMichaelAdams.java
@@ -10,22 +10,12 @@ public class MyLinkedListMichaelAdams <T> {
 	Node tail;
 	int size;
 
-	public class Node{
-		T data;
-		Node link;
-		Node(){
-		}
-		Node(T element){
-			data = element;
-			link = null;
-		}
-	}
-	
 	MyLinkedListMichaelAdams(){
 		head = null;
 		tail = null;
 		size = 0;
 	}
+	
 	public T getFirst() throws RuntimeException{
 		if (head == null) {
 			throw new RuntimeException("Head is null");
@@ -33,6 +23,7 @@ public class MyLinkedListMichaelAdams <T> {
 			return head.data;
 		}
 	}
+
 	public T getLast() throws RuntimeException{
 		if (tail == null) {
 			throw new RuntimeException("Tail is null");
@@ -40,6 +31,7 @@ public class MyLinkedListMichaelAdams <T> {
 			return tail.data;
 		}
 	}
+
 	public void addLast(T newElement){
 		Node newNode = new Node(newElement);
 		if(size==0) {
@@ -52,29 +44,23 @@ public class MyLinkedListMichaelAdams <T> {
 	}
 
 	public void removeLast(){
-		// Case 1: if the list is empty --> throw any Exception
-		if (tail == null) {
-			try {
-				throw new Exception("List is empty");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			// Case 2: if you have only one element
-		} else if (tail == head) {
-			tail = null;
+		if(head == null) {
+			throw new RuntimeException("in removeLast(): no elements in the list");
+		}
+		else if(head == tail) { // if(size==1)
+			head = tail = null;
 			size --;
-			// Case 3: in general case
-		} else {
-			Node secondToLast = head;
-			while (secondToLast.link.link != null) {
-				secondToLast = secondToLast.link;
-				secondToLast.link = null;
+		}
+		else {
+			Node current = head;
+			while (current.link != tail) {
+				current = current.link;
 			}
-			tail = tail.link;
-			size--;
+			current.link = null;
+			tail = current;
 		}
 	}
-	
+
 	public int indexOf(T targetElement){
 		// search the targetElement in the list, return the index of given targetElement if it exists.
 		int index = 0;
@@ -88,41 +74,12 @@ public class MyLinkedListMichaelAdams <T> {
 			current = current.link;
 		}
 		return -1;
-		// if the list doesn't have targetElement, return -1
-		// Caution: index starts with 0  (the first element's index is 0)
-		// Caution: to return index, you must check the index of node while you searching
-		// Write code here
 	}
+	
 	public Iterator iterator(){
 		return new Iterator();
 	}
-	
-	// Complete the Iterator class
-	class Iterator {
-		Node next; // to point [next node] object
-		
-		Iterator(){
-			// next must be the first node of the list
-                	next = head;
-		}
 
-		public T next(){
-			 // return the data_field of [next node]
-                	T data_field = next.data;
-                	next = next.link;
-                	return data_field;		
-		}
-		public boolean hasNext(){
-			// return true when the [next node] exists
-                	// return false when we don't have the [next node]
-                	if (next != null) {
-                	    return true;
-               	 	}
-                	return false;
-		}
-		
-	}
-	
 	public void removeFirst() throws RuntimeException {
 		if(head == null) {
 			throw new RuntimeException("in removeFirst(): no elements in the list");
@@ -135,8 +92,9 @@ public class MyLinkedListMichaelAdams <T> {
 			head = head.link;
 			size--;
 		}
-		
+
 	}
+	
 	public void remove(int index) {
 		if(index == 0) {
 			removeFirst();
@@ -144,7 +102,7 @@ public class MyLinkedListMichaelAdams <T> {
 		else if(head == tail) {
 			head = tail = null;
 			size--;
-		} 
+		}
 		else {
 			Node cur = head;
 			while (--index > 0) {
@@ -167,7 +125,7 @@ public class MyLinkedListMichaelAdams <T> {
 		}
 		size++;
 	}
-	
+
 	public void add(int index, T newElement) {
 		if(index == 0)
 			addFirst(newElement);
@@ -190,15 +148,52 @@ public class MyLinkedListMichaelAdams <T> {
 	public String toString() {
 		String str = "[";
 		Node temp = head;
-		
+
 		while(temp != null) {
 			str = str + temp.data;
 			if(temp != tail) {
 				str = str + ", ";
 			}
 			temp = temp.link;
-		}		
+		}
 		return str = str + "]";
+	}
+	
+	public class Node{
+		T data;
+		Node link;
+		Node(){
+		}
+		Node(T element){
+			data = element;
+			link = null;
+		}
+	}
+	
+	// Complete the Iterator class
+	class Iterator {
+		Node next; // to point [next node] object
+
+		Iterator(){
+			// next must be the first node of the list
+                	next = head;
+		}
+
+		public T next(){
+			 // return the data_field of [next node]
+                	T data_field = next.data;
+                	next = next.link;
+                	return data_field;
+		}
+		public boolean hasNext(){
+			// return true when the [next node] exists
+                	// return false when we don't have the [next node]
+                	if (next != null) {
+                	    return true;
+               	 	}
+                	return false;
+		}
+
 	}
 	
 }
