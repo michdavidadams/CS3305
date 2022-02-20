f833e35086ec788d343ddff913cda8a303959424
Michael Adams <me@michdavidadams.com>
2022-02-19 19:10:02 -0500

    arraylist

--- assignmentthree/MyStackMichaelAdams.java
+++ assignmentthree/MyStackMichaelAdams.java
@@ -1,10 +1,11 @@
 package assignmentthree;
 // Class:			Data Structures W01
-
 // Term:			Spring 2022
 // Name:			Michael Adams
 // Program Number:	3
-// IDE: 			Visual Studio Code 1.63.2
+// IDE: 			Visual Studio Code 1.64.2
+
+import java.util.ArrayList;
 
 public class MyStackMichaelAdams<E> {
     // Current size of stack
@@ -12,20 +13,20 @@ public class MyStackMichaelAdams<E> {
     // Maximum size of stack
     int MAX_SIZE;
     // Data array
-    E[] S;
+    ArrayList<E> S;
     // Index of top
-    int top;
+    E top;
 
     MyStackMichaelAdams() {
         size = 0;
         MAX_SIZE = 0;
-        S={0};
-        top = 0;
+        top = null;
+        S = new ArrayList<>();
     }
 
     // Returns true if stack is empty
     public boolean isEmpty() {
-        if (size == 0) {
+        if (S.isEmpty()) {
             return true;
         }
         return false;
@@ -38,40 +39,42 @@ public class MyStackMichaelAdams<E> {
 
     // Returns top element in stack
     public E peek() {
-        if (size != 0) {
-            return S[top];
+        if (size == 0) {
+            throw new RuntimeException("Stack is empty");
         }
-        return null;
+        return S.get(0);
     }
 
     // Returns and removes top element in stack
     public E pop() {
         if (!isEmpty()) {
-            top = top - 1;
+            top = S.get(size - 1);
+            S.remove(size - 1);
             size--;
         }
-        return S[top];
+        return S.get(size - 1);
     }
 
     // Adds new element to top of stack
-    public E push(E element) {
-        if (size <= MAX_SIZE) {
-            top = top + 1;
-            S[top] = element;
-            size++;
+    public E push(E element) throws RuntimeException {
+        if (size >= MAX_SIZE) {
+            throw new RuntimeException("Max size reached.");
         }
-        return null;
+        top = element;
+        S.add(element);
+        size++;
+        return element;
     }
 
     // Returns string containing all elements in stack
-    public String toString() {
-        if (!isEmpty()) {
-            String stringStack = "";
-            for (E i : S) {
-                stringStack += i + " ";
-            }
-            return stringStack;
+    public String toString() throws RuntimeException {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
         }
-        return null;
+        String stringStack = "";
+        for (E i : S) {
+            stringStack += i + " ";
+        }
+        return stringStack;
     }
 }
---
+++ assignmentthree/TestStackMichaelAdams.java
@@ -0,0 +1,45 @@
+package assignmentthree;
+// Class:			Data Structures W01
+
+// Term:			Spring 2022
+// Name:			Michael Adams
+// Program Number:	3
+// IDE: 			Visual Studio Code 1.64.2
+
+import java.util.Scanner;
+
+public class TestStackMichaelAdams {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Holds sentinel value
+        int userInput;
+
+        do {
+        System.out.println("-----MAIN MENU-----");
+        System.out.println("0 - Exit Program");
+        System.out.println("1 – Push");
+        System.out.println("2 - Pop");
+        System.out.println("3 – Peek (Top)");
+        System.out.println("4 - Size");
+        System.out.println("5 – Is Empty?");
+        System.out.println("6 – Print Stack");
+        System.out.println("Choose menu:");
+
+        userInput = input.nextInt();
+
+        } while(userInput != 0);
+    }
+
+    public class ExprMichaelAdams {
+        public static String infixToPostfix(String infix){
+           TestStackMichaelAdams stackOne = new TestStackMichaelAdams();
+           
+
+
+
+            // Return postfix as a returned String of this method
+            }
+            
+    }
+}
